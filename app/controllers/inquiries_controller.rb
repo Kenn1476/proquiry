3d9d2f8ec8168d6336b05227f12e5e4a31592ff2
Kenn1476 <97831182+Kenn1476@users.noreply.github.com>
2024-02-15 19:50:11 +0900

    1st Commit

---
+++ app/controllers/inquiries_controller.rb
@@ -0,0 +1,41 @@
+class InquiriesController < ApplicationController
+  def index
+    @inquiries = Inquiry.all
+  end
+
+  def new
+    @inquiry = Inquiry.new
+  end
+
+  def create
+    Inquiry.create(inquiry_params)
+    redirect_to '/'
+  end
+
+  def destroy
+    inquiry = Inquiry.find(params[:id])
+    inquiry.destroy
+    redirect_to root_path
+  end
+
+  def edit
+    @inquiry = Inquiry.find(params[:id])
+  end
+
+  def update
+    inquiry = Inquiry.find(params[:id])
+    inquiry.update(inquiry_params)
+    redirect_to root_path
+  end
+
+  def show
+    @inquiry = Inquiry.find(params[:id])
+  end
+
+  private
+  def inquiry_params
+    params.require(:inquiry).permit(:name, :image, :text)
+  end
+
+
+end
